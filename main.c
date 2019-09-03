4b550858d08a7f864ee98f796629e1d3cf5927a7
Nathan Eloe <powerofazure@gmail.com>
2019-09-03 10:16:29 -0500

    Write isprime function

--- main.c
+++ main.c
@@ -2,9 +2,21 @@
 // stdio (standard IO)
 #include <stdio.h>
 
+int is_prime(int n);
+
 int main(int argc, char* argv[])
 {
 	for (int i=0; i<10; i++)
-		printf("Hello world\n");
+	{
+		//printf("Hello world\n");
+		printf("%d is prime? %d\n", i, is_prime(i));
+	}
 	return 0;
+}
+
+int is_prime(int n)
+{
+	for (int i=2; i*i<=n; i++)
+		if (n%i == 0) return 0;
+	return 1;
 }
